681baa90c65826175b1ce9c729a460e3bf2962d7
Muhammad Afnan <muhammadafnan849@gmail.com>
2025-11-14 15:29:50 +0500

    dev

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,17 +1,87 @@
-import React from 'react'
-import './Todo.css'
+import { useState } from "react";
+import "./Todo.css";
+import { TodoForm } from "./TodoForm";
+import { TodoList } from "./TodoList";
+import { TodoDate } from "./TodoDate";
+import {
+    getLocalStorageTodoData,
+    setLocalStorageTodoData,
+} from "./TodoLocalStorage";
 
-const Todo = () => {
+export const Todo = () => {
+    const [task, setTask] = useState(() => getLocalStorageTodoData());
+
+    const handleFormSubmit = (inputValue) => {
+        const { id, content, checked } = inputValue;
+
+        //to check if the input field is empty or not
+        if (!content) return;
+
+        // to check if the data is already existing or not
+        // if (task.includes(inputValue)) return;
+        const ifTodoContentMatched = task.find(
+            (curTask) => curTask.content === content
+        );
+        if (ifTodoContentMatched) return;
+
+        setTask((prevTask) => [...prevTask, { id, content, checked }]);
+    };
+
+    //todo add data to localStorage
+    setLocalStorageTodoData(task);
+
+    //todo handleDeleteTodo function
+    const handleDeleteTodo = (value) => {
+        const updatedTask = task.filter((curTask) => curTask.content !== value);
+        setTask(updatedTask);
+    };
+
+    //todo handleClearTodoData functionality
+    const handleClearTodoData = () => {
+        setTask([]);
+    };
+
+    //todo handleCheckedTodo functionality
+    const handleCheckedTodo = (content) => {
+        const updatedTask = task.map((curTask) => {
+            if (curTask.content === content) {
+                return { ...curTask, checked: !curTask.checked };
+            } else {
+                return curTask;
+            }
+        });
+        setTask(updatedTask);
+    };
+
     return (
-        <div>
-            <h1>Todo App</h1>
-            <div className='date-time-container'>
-                <input type="date" id='date' />
-                <input type="time" id='time' />
-            </div>
-            <div>felds</div>
-        </div>
-    )
-}
+        <section className="todo-container">
+            <header>
+                <h1>Todo List</h1>
+                <TodoDate />
+            </header>
 
-export default Todo
+            <TodoForm onAddTodo={handleFormSubmit} />
+
+            <section className="myUnOrdList">
+                <ul>
+                    {task.map((curTask) => {
+                        return (
+                            <TodoList
+                                key={curTask.id}
+                                data={curTask.content}
+                                checked={curTask.checked}
+                                onHandleDeleteTodo={handleDeleteTodo}
+                                onHandleCheckedTodo={handleCheckedTodo}
+                            />
+                        );
+                    })}
+                </ul>
+            </section>
+            <section>
+                <button className="clear-btn" onClick={handleClearTodoData}>
+                    Clear all
+                </button>
+            </section>
+        </section>
+    );
+};
